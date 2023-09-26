bf6f1d0078105bf51979fc2bab0145255fbb445c
VIKRAM <VIKRAMAYER19@GMAIL.COM>
2023-09-26 17:39:01 +0530

    Add files via upload

---
+++ API.js
@@ -0,0 +1,39 @@
+document.getElementById("show").addEventListener("click",async ()=>{
+    let res = await fetch("https://datausa.io/api/data?drilldowns=Nation&measures=Population")
+    let data = await res.json()
+        console.log(data)
+        let ans = data.data
+        console.log(ans)
+        for(i of ans){
+            for(j in i){
+                let title = document.createElement('div')
+                title.setAttribute("class","title")
+                title.innerHTML = j
+                title.setAttribute("style","display:inline-block;color:blue;margin:10px; ")
+                let answer = document.createElement('span')
+                answer.innerHTML = i[j]
+                answer.setAttribute("style","margin:10px;margin-left:3px;color:green" )
+                // answer.setAttribute("style","width:20%;")
+                let screen = document.getElementById("screen")
+                let section = document.createElement("section")
+                section.append(title,answer)
+                screen.append(section)
+                section.setAttribute("style","width:15% " )
+                screen.setAttribute("style","display:flex;flex-wrap:wrap;justify-content:space-around")
+                // console.log(j)
+                console.log(j,":",i[j])
+            }
+        }
+        // let face = document.createElement("div")
+        // face.innerHTML = data.fact
+        // let screen = document.getElementById("screen")
+        // screen.append(face)
+
+    //  .then((res)=> res.json)
+    //  .then((data)=> {
+        // let face = document.createElement("div")
+        // face.innerHTML = Data.
+        // show.append(face)
+    //  })
+
+})
